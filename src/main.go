7dbb21683f7d459ac4b893d690e9fa8056983117
sk0fl1 <you.sk0fl1@gmail.com>
2020-05-15 00:01:18 +0300

    Added functions instead of code in main

--- src/main.go
+++ src/main.go
@@ -7,18 +7,66 @@ import (
 	"time"
 )
 
-var game_board = [3][3]string{}
-var q = 1
-var player_choice int
-var point string
-
 type coord struct {
 	X int
 	Y int
 }
-
+var game_board = [3][3]string{}
+var while_count = 1
+var player_choice int
+var point string
 var selected_coord coord
 
+func player_step() {
+	can := 1
+	//Players time
+	for can != 0 {
+		fmt.Println()
+		coord_picking()
+		if can_place_point() == true {
+			game_board[selected_coord.X][selected_coord.Y] = point
+			fmt.Println(point)
+			can = 0
+		}
+	}
+}
+func ai_step() {
+	can := 1
+	//AI time
+	for can != 0 {
+		selected_coord.X = rand.Intn(3-0) + 0
+		selected_coord.Y = rand.Intn(3-0) + 0
+		if can_place_point() == true {
+			game_board[selected_coord.X][selected_coord.Y] = point
+			can = 0
+		}
+	}
+	show_board()
+	win_cheking()
+}
+func create_board() {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			game_board[i][j] = "| |"
+		}
+	}
+}
+func point_swicher() {
+	if point == "|X|" {
+		point = "|O|"
+	} else {
+		point = "|X|"
+	}
+}
+func show_board() {
+	for i := 0; i < 3; i++ {
+		fmt.Println()
+		for j := 0; j < 3; j++ {
+			fmt.Print(game_board[i][j] + " ")
+		}
+	}
+	fmt.Println()
+}
 func coord_picking() coord {
 	z := 3
 	for z == 3 {
@@ -61,13 +109,13 @@ func win_cheking() bool {
 	return answer
 }
 func point_select() int {
-	for q == 1 {
+	for while_count == 1 {
 		fmt.Println("Write 1, to play by X, or write 2, to play by O")
 		fmt.Fscan(os.Stdin, &player_choice)
 		if player_choice != 1 && player_choice != 2 {
-			q = 1
+			while_count = 1
 		} else {
-			q = 0
+			while_count = 0
 		}
 	}
 	return player_choice
@@ -81,21 +129,8 @@ func can_place_point() bool {
 }
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	//Creating game board
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			game_board[i][j] = "| |"
-		}
-	}
-	//Showing game board 1st time
-	for i := 0; i < 3; i++ {
-		fmt.Println()
-		for j := 0; j < 3; j++ {
-			fmt.Print(game_board[i][j] + " ")
-		}
-	}
-	fmt.Println()
-	point_select()
+	create_board()
+	show_board()
 	if point_select() == 1 {
 		point = "|X|"
 	} else {
@@ -104,24 +139,8 @@ func main() {
 	fmt.Println("U picked " + point)
 	win := 0
 	for win != 1 {
-		can := 1
-		//Players time
-		for can != 0 {
-			fmt.Println()
-			coord_picking()
-			if can_place_point() == true {
-				game_board[selected_coord.X][selected_coord.Y] = point
-				fmt.Println(point)
-				can = 0
-			}
-		}
-		for i := 0; i < 3; i++ {
-			fmt.Println()
-			for j := 0; j < 3; j++ {
-				fmt.Print(game_board[i][j] + " ")
-			}
-		}
-		win_cheking()
+		player_step()
+		show_board()
 		if win_cheking() == true {
 			win = 1
 			fmt.Println()
@@ -129,39 +148,14 @@ func main() {
 		}
 		fmt.Println()
 		//changing point
-		if point == "|X|" {
-			point = "|O|"
-		} else {
-			point = "|X|"
-		}
-		can = 1
-		//AI time
-		for can != 0 {
-			selected_coord.X = rand.Intn(3-0) + 0
-			selected_coord.Y = rand.Intn(3-0) + 0
-			if can_place_point() == true {
-				game_board[selected_coord.X][selected_coord.Y] = point
-				can = 0
-			}
-		}
-		for i := 0; i < 3; i++ {
-			fmt.Println()
-			for j := 0; j < 3; j++ {
-				fmt.Print(game_board[i][j] + " ")
-
-			}
-		}
-		win_cheking()
+		point_swicher()
+		ai_step()
 		if win_cheking() == true {
 			win = 1
 			fmt.Println()
 			fmt.Println(point + " Win!")
 		}
 		//Changing point again
-		if point == "|X|" {
-			point = "|O|"
-		} else {
-			point = "|X|"
-		}
+		point_swicher()
 	}
 }
